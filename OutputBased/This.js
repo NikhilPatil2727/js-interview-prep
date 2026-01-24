9f5dd59e9bb6ee74980c7972c640a00d5693d9ca
NikhilPatil2727 <nikhilpatil02436@gmail.com>
2026-01-24 12:37:02 +0530

    Update This Keyword Module

--- OutputBased/This.js
+++ OutputBased/This.js
@@ -20,31 +20,53 @@
 //     }
 // }
 
+// function makeUser(){
+//     return {
+//         name:"nikhil",
+//         ref(){
+//           return this.name
+//         }
+//     }
+// }
+
+
 // let user=makeUser();
 
-// console.log(user.ref.name);
+// console.log(user.ref());
 
 ////******************************************************************** */
 
-const User = {
-  name: "NIKHIL PATIL",
-  logMessage: function () {
-    console.log(this.name);
-  },
-};
+// const User = {
+//   name: "NIKHIL PATIL",
+//   logMessage: function () {
+//     console.log(this.name);
+//   },
+// };
 
-// setTimeout(User.logMessage,5000);
+// setTimeout(User.logMessage,1000);
+
 // called wtihout object
+
 // setTimeout(() => {
-//   User.logMessage();
-// }, 5000);
-//the function is called with object
+//   User.logMessage
+// }, 1000);
 
-// You are passing only the function, not the object.
 
-// When setTimeout calls it, this does not refer to User anymore.
+// User.logMessage   // function exists, but not executed
+// User.logMessage() // function runs ✅
 
-// So this.name becomes undefined.
+// I am passing only reference, not calling it”
+
+// This line just returns the function reference.
+// It does NOT execute it.
+
+// A function reference cannot execute by itself.
+// It must be CALLED using () to run.
+
+//It will Pointing to the window object it will point Sepertly running like 
+
+
+// “It points to the window object because the method is executed separately, not as part of the User object.”
 
 ////******************************************************************** */
 
@@ -54,10 +76,12 @@ const User = {
 // let calculator = {
 // //   a: 20,
 // //   b: 30,
+
+// // understand why the this.a and this.b is written inside the function
 //   read: function () {
 //     // console.log(this.a + " " + this.b);
-//       this.a=prompt("a")
-//       this.b=prompt("b")
+//        this.a=Number(prompt("a"))
+//        this.b=Number(prompt("b"))
 
 //   },
 //   sum: function () {
@@ -71,6 +95,8 @@ const User = {
 // calculator.read();
 // console.log(calculator.sum());
 // console.log(calculator.mul());
+
+
 ////******************************************************************** */
 
 
@@ -82,7 +108,6 @@ const User = {
 // }
 
 
-
 // const object={
 //     length:5,
 //     Greet:function(callback){
@@ -91,7 +116,56 @@ const User = {
 // }
 // object.Greet(callback);
 
-//It prints 4 because callback() is called as a normal function, not as a method of object
+
+
+// Is detached from object
+
+// Runs independently
+
+// Is not a method call
+
+// The callback function is executed separately, not as a method of the object, so this refers to the global object.
+
+//******************************************************************** */
+
+//Senior Engineer question 
+
+// var length=4;
+
+
+// function callback(){
+//     console.log(this.length)
+// }
+
+
+// const object={
+//     length:5,
+//     Greet:function(callback){
+//       console.log(arguments)
+//         arguments[0]()
+//     }
+// }
+// object.Greet(callback,1,2);
+
+// in internally they where saved as object
+
+// because
+// arguments = {
+//   0: callback,
+//   1: 1,
+//   2: 2,
+//   length: 3
+// }
+
+//that wahy the outptu is 3 
+
+// arguments is an object that behaves like an array, and since the function is called using arguments[0](), this points to the arguments object.
+// Because arguments.length is 3, the output is 3.
+
+
+
+
+
 
 
 ////******************************************************************** */
