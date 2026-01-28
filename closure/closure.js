f7a14fd62f009c72623c9c35392dbae038f1cb38
NikhilPatil2727 <nikhilpatil02436@gmail.com>
2026-01-28 12:17:04 +0530

    updated some modules

--- closure/closure.js
+++ closure/closure.js
@@ -1,4 +1,25 @@
 
+//clousre having a access of global variable 
+
+// global scope
+const e = 10;
+function sum(a) {
+  return function (b) {
+    return function (c) {
+      // outer functions scope
+      return function (d) {
+        // local scope
+        return a + b + c + d + e;
+      };
+    };
+  };
+}
+
+console.log(sum(1)(2)(3)(4)); // 20
+
+
+
+
 
 // function greet(){
 //     let name="NIKHIL"
